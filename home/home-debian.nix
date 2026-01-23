5f6e4420cf9a5811f18fdb693cca2b9902539e78
marcuslannister <marcus@lannister.cc>
2026-01-23 21:13:21 +0800

    feat: add zellij and more

--- home/home-debian.nix
+++ home/home-debian.nix
@@ -5,13 +5,66 @@
   # Import the base home configuration
   imports = [ ./home.nix ];
 
+  programs.zsh = {
+    enable = true;
+    enableCompletion = false; # <--- disables the default compinit
+    # You can still use shellInit or other options as needed
+  };
+
   # Add specific packages on top of the base ones
   home.packages = with pkgs; [
+    # base
+    vim
     git
-    zsh
-    eza
-    zoxide
+    ruby # for ~/.scm_breeze/install.sh
+    # jujutsu
+    nodejs_24
+
+
+    # python
+    (python3.withPackages (ps: with ps; [
+      pip
+      setuptools
+      wheel
+    ]))
+
+
+    # utils
+    eza # A modern replacement for ‘ls’
+    ripgrep # recursively searches directories for a regex pattern
     tmux
+    zellij
+    ncdu
+    bc
+
+    # files
+    duf
+    dust
+    fzf
+    fd
+
+    # misc
+    zoxide
     delta
+    # bitwarden-cli
+
+    # network
+    iperf3
+    croc
+    axel
+
+    # document
+    pandoc
+    tectonic
+    # pdflatex
+    # aspell
+    # aspellDicts.en
+    (aspellWithDicts (dicts: with dicts; [en en-computers en-science]))
+
+    # cmake
+    # unstable.syncthing
+
+    # docker
+    docker-compose
   ];
 }
